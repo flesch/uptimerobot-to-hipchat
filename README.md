5d15343e41c9ae4aeaae8aa1b901d41a999564a2
John Flesch <john@fles.ch>
2014-05-31 22:21:55 -0500

    Initial commit.

---
+++ README.md
@@ -0,0 +1,7 @@
+# uptimerobot-to-hipchat
+
+A webhook service to post Uptime Robot alerts to a HipChat room.
+
+![hipchat](https://cloud.githubusercontent.com/assets/13259/3140812/0c3daffc-e940-11e3-8bbd-599fe54f6df4.png)
+
+    http://uptimerobot-to-hipchat.herokuapp.com/[hipchat api token]/[room id]?
---
+++ server.js
@@ -0,0 +1,37 @@
+var express = require('express')
+  , request = require('request')
+  , util = require('util')
+  , packagejson = require('./package.json')
+  ;
+
+var app = express();
+
+app.get('/:token/:room', function(req, res, next){
+
+  if (req.params.token && req.params.room) {
+
+    var payload = {
+      auth_token: req.params.token,
+      room_id: req.params.room,
+      from: 'UptimeRobot',
+      message: util.format('"%s" (%s) is %s! (%s)', req.query.monitorFriendlyName, req.query.monitorURL, (req.query.alertType === 1 ? 'down' : 'up'), req.query.alertDetails),
+      message_format: 'text',
+      notify: req.query.alertType === 1,
+      color: (req.query.alertType === 1) ? 'red' : 'green'
+    };
+
+    request.post({ url:"https://api.hipchat.com/v1/rooms/message", qs:payload, json:true });
+
+  }
+
+  res.json({ name:packagejson.name, version:packagejson.version });
+
+});
+
+app.get('/*', function(req, res, next){
+  res.redirect('https://github.com/flesch/uptimerobot-to-hipchat');
+});
+
+var server = app.listen(process.env.PORT || 3000, function(){
+  console.log('> %s (v%s) listening on port %d', packagejson.name, packagejson.version, server.address().port);
+});
